708f2eb8a73e108ad3601fcbc81a9b222a6212ff
arjunnoobz <50669518+arjunnoobz@users.noreply.github.com>
2019-05-15 22:45:44 +0700

    Add files via upload

--- tampan.sh
+++ tampan.sh
@@ -1,1 +1,688 @@
+# Mau Apa Lu anjing...
+#Masa  Script Kek gini Aja di Recorde
 
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+
+
+
+
+figlet LOADING...
+sleep 0.90
+sleep 0.90
+sleep 0.90
+clear
+echo
+echo
+python2 meizu.py
+echo
+clear
+
+
+
+sleep 0.90
+echo $green"[====================================================]"
+
+figlet SELAMAT DATANG
+
+echo $green"[====================================================]"
+sleep 0.90
+sleep 0.90
+clear
+echo $purple"[>================================================>]"
+sleep 0.80
+figlet Tools Installer
+sleep 0.80
+echo $purple"[>===============================================>]"
+
+
+echo $blue"<--------------------------------------------------->"
+sleep 0.90
+echo $blue"<                BY : ARJUN_24                      >"
+sleep 0.90
+echo $blue"<                                                   >"
+sleep 0.90
+echo $blue"<              TEAM : Anonymous INDONESIA           >"
+sleep 0.90
+echo $blue"<                                                   >"
+sleep 0.90
+echo $blue"<             FROM  : SEMANDING                     >"
+sleep 0.90
+echo $blue"<                                                   >"
+sleep 0.90
+echo $blue"<            SCHOOL : MTS                           >"
+sleep 0.90
+echo $blue"<                                                   >"
+sleep 0.90
+echo $blue"<            THANKS TO : ANONYMOUS INDONESIA        >"
+sleep 0.90
+echo $blue"<--------------------------------------------------->"
+sleep 0.90
+
+echo $green"######################################################"
+
+
+echo $cyan"                 ----------------------------"
+sleep 0.90
+echo $red"                  Please Support My Program"
+sleep 0.90
+echo $cyan"                 ----------------------------"
+echo $blue"          :::::::::::::::::::::::::::::::::::::::::::"
+
+echo $purple"        Bikin Sc Ngga Semudah Bilang Love you tau :v"
+
+echo $blue"          :::::::::::::::::::::::::::::::::::::::::::"
+sleep 0.90
+echo $blue"               ?      -------------    ?"
+sleep 0.90
+echo $blue"               \ \   |             |  //"
+sleep 0.90
+echo $blue"                \ \  |  ^       ^  | //"
+sleep 0.90
+echo $cyan"                 \ \ |     ----    |//""PILIH TOOLS YANG LU SUKA"
+sleep 0.90
+echo $cyan"                  \  --------------/""JANGAN KOMEN KALO LU NGGA BISA"
+sleep 0.90
+echo $blue"                     :             :""BIKIN"
+sleep 0.90
+echo $cyan"                     :             :""BY:ARJUN TAMPAN_24"
+sleep 0.90
+echo $blue"                     :             :""                 My CONTACT :"
+sleep 0.90
+echo $cyan"                     ::::::::::::::""                 083869752666"
+sleep 0.90
+echo $purple"                     : :      : :"
+sleep 0.90
+echo $purple"                     : :      : :"
+sleep 0.90
+echo $purple"                     : :      : :"
+sleep 0.90
+echo $green"                    :   :    :    :"
+sleep 0.90
+echo $green"                    .....     ....."
+sleep 0.90
+
+
+echo $purple"         :::::::::::::::::::::"
+echo $purple"         :                   :"
+echo $purple"         :   ArJun TaMVanS   :"
+echo $purple"         :                   :"
+echo $purple"         ••••••••••••••••••••"
+echo $purple"         • Jomblo Mah Bebas •"
+echo $purple"          •••••••| |•••••••"
+echo $purple":::::::::::::::::::::::::::::::::"
+echo $purple"_________________________________"
+echo $purple"[1] Spam Work"
+echo $purple"_________________________________"
+echo $purple"[2] HACK FB TARGET"
+echo $purple"_________________________________"
+echo $purple"[3] Installer FBH"
+echo $purple"_________________________________"
+echo $purple"[4] Tools Rahmad"
+echo $purple"_________________________________"
+echo $purple"[5] Yahoo Vuln"
+echo $purple"_________________________________"
+echo $purple"[6] Spam Call "
+echo $purple"_________________________________"
+echo $purple"[7] Sqlmap"
+echo $purple"_________________________________"
+echo $purple"[8] Hack CCTV"
+echo $purple"_________________________________"
+echo $purple"[9] HACK Email"
+echo $purple"_________________________________"
+echo $purple"[10]Pulsa Free Gratis All"
+echo $purple"_________________________________"
+echo $purple"[11] Mempercantik termux"
+echo $purple"_________________________________"
+echo $purple"[12] BOT FB OSIF"
+echo $purple"_________________________________"
+echo $purple"[13] Macam² Hacking       "
+echo $green"__________________________________"
+echo $green"[14] RED_HAWK"
+echo $green"__________________________________"
+echo $green"[15] Lacak Ip"
+echo $green"__________________________________"
+echo $green"[16] Membuat Sc deface Sendiri"
+echo $green"__________________________________"
+echo $green"[17] INSTALL A-Rat Buat Hack Hp"
+echo $green"__________________________________"
+echo $green"[18] INSTALL TOOLS Mr.Rv1.1"
+echo $green"__________________________________"
+echo $green"[19] Tools BAJINGANv6"
+echo $green"__________________________________"
+echo $green"[20] HACK CEWE BUAT JONES"
+echo $green"__________________________________"
+echo $green"[21] Virtex Leg Wa "
+echo $green"__________________________________"
+echo $green"[22] Hack Fb MBF"
+echo $green"__________________________________"
+echo $green"[23] Spam Caping Work"
+echo $green"__________________________________"
+echo $blue"[24] Spam Trol HACK BAE"
+echo $blue"___________________________________"
+read -p "[arjun_tampanz] pilih no=>" pill;
+
+if [ $pill = 1 ]
+then
+clear
+figlet Waiting...
+sleep 1
+pkg install git
+git clone https://github.com/zlucifer/sms_bomb
+cd sms_bomb
+chmod 755 bom.sh
+termux-fix-shebang bom.sh
+./bom.sh
+fi
+
+
+if [ $pill = 2 ]
+then
+clear
+figlet Waiting...
+sleep 1
+apt install git
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+chmod +x brute.py
+python2 brute.py
+fi
+
+if [ $pill = 3 ]
+then
+clear
+echo $blue"•••••••••••••••••••••••••••••••••••••••••••••••••••••"
+figlet ARJUN_24
+echo $blue"•••••••••••••••••••••••••••••••••••••••••••••••••••••"
+sleep 1
+clear
+figlet Loading...
+apt install git
+git clone https://github.com/muhammadfathul/FBH
+cd FBH
+chmod +x *
+bash setup
+fi
+
+
+if [ $pill = 4 ]
+then
+clear
+figlet ARJUN24
+sleep 1
+clear
+echo $blue"user:Rahmad
+echo $blue"pass:Jomblo123
+pkg install git
+git clone https://github.com/rahmadxyz/ToolsLengkap
+cd ToolsLengkap
+sh rahmad.sh
+fi
+
+
+if [ $pill = 9 ]
+then
+clear
+echo $green"----------------------------------------------------"
+figlet Hack Email
+echo $green"----------------------------------------------------"
+echo $cyan"----------------------------------------------------"
+sleep 0.90
+echo $cyan"-           Program : ARJUN NOOBZ                  -"
+sleep 0.90
+echo $cyan"-                                                  -"
+sleep 0.90
+echo $cyan"-           Team    : Cyber Hacker                 -"
+sleep 0.90
+echo $cyan"-                                                  -"
+sleep 0.90
+echo $cyan"-          thanks to: Anonymous indonesia          -"
+sleep 0.90
+echo $cyan"----------------------------------------------------"
+sleep 0.90
+sleep 0.90
+sleep 0.90
+pkg update
+pkg upgrade
+clear
+echo
+echo $blue"[Masukan No Telpon=>]"
+read telpon
+echo
+sleep 0.90
+sleep 0.90
+echo $green"[Masukan Email anda=>]"
+read anda
+echo
+sleep 0.80
+
+sleep 0.90
+sleep 0.90
+clear
+figlet Proses..
+sleep 0.90
+sleep 0.90
+sleep 0.90
+sleep 0.90
+sleep 0.90
+sleep 0.90
+
+clear
+figlet Success!!
+sleep 0.90
+sleep 0.90
+echo $purple"[35 Email Success! Terhack]"
+sleep 0.50
+echo "[Betari Aisah] >> jengbet20@gmail.com"
+sleep 0.50
+echo "[Fajar Heri Mahbubi] >> fmahbubi@gmail.com"
+sleep 0.50
+echo "[Iyans Mochammad] >> mochammad.sofiyan@yahoo.com"
+sleep 0.50
+echo "[Celvy Ramadhaniie] >> celvymued@rocketmail.com"
+sleep 0.50
+echo "[Iazz Tieaz] >> tyaswahyu80@yahoo.com"
+sleep 0.50
+echo "[Nanda Ayu] >> deviarieni@yahoo.com"
+sleep 0.50
+echo "[Rere Sedulur Papat] >> reyzreyz90@yahoo.co.id"
+sleep 0.50
+echo "hendrikchoky@yah[Chen Joephe] >> saya.adalah46@yahoo.com"
+sleep 0.50
+echo "[Humaira Phinata Zahra] >> humairaphinatazahra@yahoo.com"
+sleep 0.50
+echo "[Rani] >> raniokynarsis@yahoo.co.id"
+sleep 0.50
+echo "[Duy] >> cakep.yuda@yahoo.com"
+sleep 0.50
+echo "[Yusnita Budiarti] >> yuse.chandra@gmail.com"
+sleep 0.50
+echo "[Vina Dayhat] >> vinadayhat@yahoo.co.id"
+sleep 0.50
+echo "[Mboh Jr.] >> muhammadhedi@yahoo.com"
+sleep 0.50
+echo "[Mely Amelia] >> melyamelia26@yahoo.co.id"
+sleep 0.50
+echo "[Bima Cahya] >> bimacahya10@yahoo.com"
+sleep 0.50
+echo "[Opsi Islami Aji] >> opsi.bonek@yahoo.com"
+sleep 0.50
+echo "[Walidatus Wilda] >> walidatus15@yahoo.co.id"
+echo $green"    Done 100% by : Arjun tamvanz"
+echo $green" Cie.... udah bisa hack email......."
+sleep 0.90
+sleep 0.90
+echo $blue" [Jangan Sombong ya bro..di atas masih ada lamgit]"
+echo $cyan"               jangan di salah gunakan...."
+fi
+
+if [ $pill = 10 ]
+then
+clear
+figlet HACK PULSA
+echo $red"                         LOADING..."
+sleep 0.90
+clear
+echo $blue"       Tools Akan Di Install Dalam Waktu"
+sleep 0.90
+figlet                              3 detik
+clear
+figlet                              2 detik
+clear
+figlet                              1 detik
+pkg install toilet
+gem install lolcat
+git clone https://github.com/InYourG00D1/PulsaFree
+ls
+cd PulsaFree
+sh Free.sh
+fi
+
+
+if [ $pill = 5 ]
+then
+clear
+figlet ARJUN24
+apt install git
+clear
+git clone https://github.com/FR13ND8/EmailVuln
+clear
+cd EmailVuln
+clear
+python2 vuln.py
+fi
+
+if [ $pill = 6 ]
+then
+clear
+figlet Waiting...
+apt install git
+git clone https://github.com/Senitopeng/MultiWa
+cd MultiWa
+unzip wa.zip
+fi
+
+if [ $pill = 7 ]
+then
+clear
+figlet Mr_x
+pkg install git -y
+git clone https://github.com/sqlmapproject/sqlmap.git
+cd sqlmap
+python2 sqlmap.py
+fi
+
+
+if [ $pill = 8 ]
+then
+clear
+figlet Mr_x
+pkg install git 
+git clone https://github.com/kancotdiq/ipcs
+cd ipcs
+python2 scan.py
+fi
+
+
+if [ $pill = 11 ]
+then
+clear
+figlet Mr_x
+pkg update && pkg upgrade
+clear
+pkg install ruby cowsay toilet figlet
+clear
+pkg install neofetch
+clear
+pkg install nano
+clear
+gem install lolcat
+clear
+cd ../usr/etc
+clear
+nano bash.bashrc
+fi
+
+
+if [ $pill = 12 ]
+then
+clear
+figlet ARJUN_24
+echo $green"User:Mbest"
+echo $blue" pass:S3MP4K"
+pkg update
+pkg upgrade
+pkg install python2
+pip2 install requests mechanize
+pkg install git
+git clone https://github.com/Ciku370/OSIF.git
+cd OSIF
+python2 osif.py
+fi
+
+if [ $pill = 13 ]
+then
+clear
+figlet Mr_x
+apt install git
+git clone https://github.com/MEiZU-M5/edi.git 
+cd edi
+sh 1.sh
+fi
+
+
+if [ $pill = 14 ]
+then
+clear
+figlet Mr_@rjun
+apt install git
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+chamod +x rhawk.php
+php rhawk.php
+fi
+
+
+if [ $pill = 15 ]
+then
+clear
+figlet Mr_x
+pkg install git
+git clone https://github.com/hatakecnk/CNK-IPFinder
+cd CNK-IPFinder
+python2 cnk-ipfinder.py
+fi
+
+
+
+
+
+if [ $pill = 16 ]
+then
+clear
+figlet waiting..
+apt install git
+clear
+git clone https://github.com/Ubaii/script-deface-creator
+clear
+cd script-deface-creator
+clear
+chmod +x create.py
+clear
+python2 create.py
+fi
+
+if [ $pill = 17 ]
+then
+clear
+figlet arjun_tamvanz
+sleep 1
+pkg install git
+git clone https://github.com/Xi4u7/A-Rat.git
+cd A-Rat
+python2 A-Rat.py
+fi
+
+
+
+
+if [ $pill = 18 ]
+then
+clear
+figlet Arjun_tamvanz
+sleep 1
+pkg install git
+pkg install gem
+pkg install figlet
+gem install lolcat
+git clone https://github.com/Mr-R225/Mr.Rv1.1
+cd Mr.Rv1.1
+sh Mr.Rv1.1.sh
+fi
+
+
+if [ $pill = 19 ]
+then
+clear
+figlet aRJUN_24
+sleep 1
+apt install git
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6.git
+cd BAJINGANv6 
+sh BAJINGAN.sh
+fi
+
+
+
+
+if  [ $pill = 20 ]
+then
+clear
+figlet HACK CEWE
+sleep 1
+apt update
+apt upgrade
+clear
+echo $blue"--------------------------------------------------"
+figlet HACK CEWE
+echo $blue"        by : Arjun Noobz"
+echo $blue"        Soory Cuma prank Doang jangan marah:v "
+echo $blue"-------------------------------------------------"
+echo $blue"Masukan No telponmu =>"
+read telponmu
+sleep 0.90
+sleep 0.90
+sleep 0.90
+echo $purple"LOADING......."
+sleep 0.90
+sleep 0.90
+sleep 0.90
+clear
+echo $green"SUKSES..."
+sleep 0.90
+sleep 0.90
+sleep 0.90
+clear
+
+echo $blue"+6283871560489 => Cantik Bro...Masih di SMP PGRI"
+sleep 0.90
+sleep 0.90
+echo $green"+62895423780408 => Cantik Bro..Masih di MTS 8 GOMBONG"
+sleep 0.90
+sleep 0.90
+echo $green"+62 812-9220-9041 => Cantik Bro..Dari Jakarta"
+sleep 0.90
+sleep 0.90
+echo $green"+62895377195946 => Cantik Masih di MTS 8 GKMBONG"
+sleep 0.90
+sleep 0.90
+echo $green"+6283144747692 => Cantii bgt masih di SMP PGRI"
+sleep 0.90
+sleep 0.90
+echo $green"+62 813-2759-8443 => Cantik Mas..Sumpah..."
+sleep 0.90
+sleep 0.90
+echo $green"+62815279368281 => Ganteng Mba....."
+sleep 0.90
+sleep 0.90
+sleep 0.90
+echo $green"+6235647983653 => Udah Lansia Gondol Aja Bro... Yang suka wkwk:v"
+sleep 0.90
+sleep 0.90
+sleep 0.90
+echo $blue"  SEMOGA DENGAN NO WA ITU LU JADI NGGA JONES YAH BRO"
+sleep 0.90
+sleep 0.90
+sleep 0.90
+echo $blue"       BYE...BYE..... PARAJOMBLO"
+
+fi
+
+
+
+
+
+
+if [ $pill = 21 ]
+then
+clear
+figlet Mr_ARJUN
+echo $blue"                         LOADING"
+sleep 0.90
+sleep 0.90
+echo $green"                 SUKSES"
+sleep 0.90
+apt install git
+
+apt install curl
+
+apt install figlet
+
+apt install ruby
+
+gem install lolcat
+
+git clone https://github.com/muhammadfathul/VIRTEX
+
+cd VIRTEX
+
+chmod +x virtex.sh
+
+sh virtex.sh
+fi
+
+
+
+if [ $pill = 22 ]
+then
+clear
+figlet Waiting...
+clear
+sleep 0.90
+sleep 0.90
+figlet HACK FB MBF
+sleep 0.90
+echo "LOADING...."
+sleep 0.90
+sleep 0.90
+clear
+pkg install git
+clear
+echo $blue"               Tools Akan di Install Dalam"
+figlet                                3 detik
+sleep 0.90
+clear
+git clone https://github.com/YukersCreew/mbf
+clear
+echo $green"                   Tunggu Sayang..."
+figlet                                  2 detik
+sleep 0.90
+clear
+cd mbf
+clear
+echo $purple"                  Sedikit Lagi Sayang...."
+figlet                                   1 detik
+sleep 0.90
+clear
+echo $cyan"               NAH TUH UDAH JADI"
+figlet                            0 detik
+clear
+python2 MBF.py
+fi
+
+
+if [ $pill = 23 ]
+then
+clear
+sleep 0.90
+pkg install curl
+ pkg install php
+pkg install git
+git clone https://github.com/alfianokt/caping-spam
+cd caping-spam
+php run.php
+fi
+
+
+if [ $pill = 24 ]
+then
+clear
+sleep 0.90
+echo $blue"________________________________________"
+echo $blue"           Mod by 4r7un_24"
+echo $blue"           Tools HACK BAE "
+echo $blue"              Work 100%"
+echo $blue"________________________________________"
+sleep 0.90
+sleep 0.90
+git clone https://github.com/zlucifer/troll_project
+cd troll_project
+chmod +x *
+termux-fix-shebang troll.sh
+./troll.sh
+fi
